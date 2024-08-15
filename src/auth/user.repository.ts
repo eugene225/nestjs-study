b3aa2156b9dc4872ee37c91dd61cd847a4838e43
eugene <81062608+eugene225@users.noreply.github.com>
2024-08-15 20:07:37 +0900

    feat : user 회원가입 구현

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,10 +1,30 @@
 import { Repository, DataSource } from 'typeorm';
 import { User } from './user.entity';
 import { CustomRepository } from 'src/global/decorator/typeorm-ex.decorator';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
   constructor(dataSource: DataSource) {
     super(User, dataSource.createEntityManager());
+  }
+
+  async createUser(authCredentialDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialDto;
+    const user = this.create({ username, password });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Exisiting username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
